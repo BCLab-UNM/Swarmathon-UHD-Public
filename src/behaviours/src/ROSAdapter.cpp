fb885eecdfc63f4b33240b527ddf5b4fa78a0a80
Antonio Griego <deano505@unm.edu>
2018-03-23 16:16:34 -0600

    Update ROSAdapter.cpp
    fix team name

--- src/behaviours/src/ROSAdapter.cpp
+++ src/behaviours/src/ROSAdapter.cpp
@@ -592,7 +592,7 @@ void joyCmdHandler(const sensor_msgs::Joy::ConstPtr& message) {
 
 void publishStatusTimerEventHandler(const ros::TimerEvent&) {
   std_msgs::String msg;
-  msg.data = "online";
+  msg.data = "UHD";
   status_publisher.publish(msg);
 }
 
